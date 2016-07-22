e704d70220e366499d577797054e96b4b3981a9e
Manish Bisht <manish.bisht490@gmail.com>
2016-07-22 20:05:40 +0530

    Solution Added

---
+++ Codeforces/Codeforces Round #360 (Div. 2)/Lovely Palindromes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main()
+{
+    cin>>s;
+    cout<<s;
+    reverse(s.begin(), s.end());
+    cout<<s;
+    return 0;
+}
