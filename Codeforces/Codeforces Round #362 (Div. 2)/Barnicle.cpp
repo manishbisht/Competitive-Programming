11b5ae0f38f0f71be96dc37eef9658773995dd8f
Manish Bisht <manish.bisht490@gmail.com>
2016-07-24 15:17:15 +0530

    Solution Added

---
+++ Codeforces/Codeforces Round #362 (Div. 2)/Barnicle.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <bits/stdc++.h>
+using namespace std;
+long long int i, a, b, len, temp;
+double dd, d;
+string s;
+int main()
+{
+	cin>>s;
+	temp=s.find(".");
+	a=stoi(s.substr(0, 2));
+	cout<<a;
+    return 0;
+}
