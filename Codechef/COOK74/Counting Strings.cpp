4366ff235859e22da5010649484a42a14a3eb2a9
Manish Bisht <manish.bisht490@gmail.com>
2016-09-24 20:00:00 +0530

    Solution Addded

---
+++ Codechef/COOK74/Counting Strings.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int i, ans;
+string s;
+int main()
+{
+    cin>>s;
+    sort(s.begin(), s.end());
+    reverse(s.begin(), s.end());
+    for(i=1;i<n;i++){
+
+    }
+    return 0;
+}
