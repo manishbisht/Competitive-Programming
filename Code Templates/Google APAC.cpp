1829305cdb0d5fb88dbf0a4aca7176a35ed850a6
Manish Bisht <manish.bisht490@gmail.com>
2016-12-11 20:53:42 +0530

    Code Template modified

--- Code Templates/Google APAC.cpp
+++ Code Templates/Google APAC.cpp
@@ -1,6 +1,12 @@
 #include <bits/stdc++.h>
-std::ifstream cin("input.in");
-std::ofstream cout("output.out");
+using namespace std;
 int main() {
+    freopen("A-large-practice.in", "r", stdin);
+	freopen("output.out", "w", stdout);
+    long long int t, n, i, j, ans;
+    cin>>t;
+    for(i=0;i<t;i++){
+        cout<<"Case #"<<t+1<<": "<<ans<<endl;
+    }
     return 0;
 }
