3fc8a5ac28ea38001b8a48b21fb5b362d3ebe52c
Manish Bisht <manish.bisht490@gmail.com>
2016-12-14 11:56:13 +0530

    topcoder srm template added

---
+++ Code Templates/Topcoder-SRM.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Initials {
+   public : string getInitials(string s){
+       string c="";
+       c.push_back(s[0]);
+       for(int i=1;i<s.length();i++){
+            if(s[i]==' '){
+                c.push_back(s[i+1]);
+            }
+	    }
+	    return c;
+	}
+};
+int main(){
+    classname t;
+    t.s();
+}
