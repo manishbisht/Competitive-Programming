cf5c9ec10cba49a3e0c3338329357f7415e8848c
Manish Bisht <manish.bisht490@gmail.com>
2016-12-15 17:12:37 +0530

    LCM Algorithm using GCD Euclid

--- Algorithms/Mathematics/LCM - Normal using GCD Euclid.cpp
+++ Algorithms/Mathematics/LCM - Normal using GCD Euclid.cpp
@@ -4,17 +4,17 @@ using namespace std;
 long long int GCDEuclid(long long int a, long long int b){
     if(b==0)
         return a;
-    return GCDEuclid(a, a%b);
+    return GCDEuclid(b, a%b);
 }
 long long int LCM(long long int a, long long int b){
     return b*a/GCDEuclid(a, b);
 }
 int main()
 {
-    for(long long int i=0;i<50;i++){
+    for(long long int i=0;i<100;i++){
         long long int a = rand() % 100;
         long long int b = rand() % 100;
-        cout<<a<<", "<<b<<" "<<GCDEuclid(a, b)<<endl;
+        cout<<a<<", "<<b<<" "<<LCM(a, b)<<endl;
     }
     return 0;
 }
