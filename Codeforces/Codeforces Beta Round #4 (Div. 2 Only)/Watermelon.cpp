b0ea1495bd4adf99eed0076680c45b4327257b8d
Manish Bisht <manish.bisht490@gmail.com>
2017-01-01 16:01:33 +0530

    Codeforces Watermelon Solution Added

---
+++ Codeforces/Codeforces Beta Round #4 (Div. 2 Only)/Watermelon.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin>>n;
+    if(n%2==0 && n>3){
+        cout<<"YES"<<endl;
+    }
+    else {
+        cout<<"NO"<<endl;
+    }
+    return 0;
+}
