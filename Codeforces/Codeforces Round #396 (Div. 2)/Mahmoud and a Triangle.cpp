8b54f548b7e6f0b7443702a20283861e6d63930c
Manish Bisht <manish.bisht490@gmail.com>
2017-02-12 22:05:42 +0530

    codeforces solution added

---
+++ Codeforces/Codeforces Round #396 (Div. 2)/Mahmoud and a Triangle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<long long int> v;
+    long long int n, i;
+    cin>>n;
+    v.resize(n);
+    for(i = 0; i < n; ++i){
+        cin>>v[i];
+    }
+    sort(v.begin(), v.end());
+    for(i = 0; i < n - 3; ++i){
+
+    }
+    return 0;
+}
