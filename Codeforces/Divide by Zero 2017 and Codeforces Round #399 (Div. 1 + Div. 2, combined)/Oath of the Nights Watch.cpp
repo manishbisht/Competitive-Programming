1639bc2556cdb9d9a9a65412d3147c9989d2b31d
Manish Bisht <manish.bisht490@gmail.com>
2017-02-20 22:58:22 +0530

    Codeforces solution added

---
+++ Codeforces/Divide by Zero 2017 and Codeforces Round #399 (Div. 1 + Div. 2, combined)/Oath of the Nights Watch.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n, i, ans, a, b;
+    vector<long long int> v;
+    cin>>n;
+    v.resize(n);
+    for(i = 0; i < n; ++i){
+        cin>>v[i];
+    }
+    sort(v.begin(), v.end());
+    a = *max_element(v.begin(), v.end());
+    b = *min_element(v.begin(), v.end());
+    ans = 0;
+    for(i = 0; i < n; ++i){
+        if(v[i] < a && v[i] > b){
+            ans++;
+        }
+    }
+    cout<<ans<<endl;
+    return 0;
+}
