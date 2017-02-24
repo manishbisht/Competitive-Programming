bdc1c157e741e34a27b2f92c79acb32e275f1b5a
Manish Bisht <manish.bisht490@gmail.com>
2017-02-24 22:23:38 +0530

    codeforces solution added

---
+++ Codeforces/Divide by Zero 2017 and Codeforces Round #399 (Div. 1 + Div. 2, combined)/Jon Snow and his Favourite Number.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n, a, b, i;
+    vector<long long int> v;
+    cin>>n>>a>>b;
+    v.resize(n);
+    for(i = 0; i < n; ++i){
+        cin>>v[i];
+    }
+    cout<<ans<<endl;
+    return 0;
+}
