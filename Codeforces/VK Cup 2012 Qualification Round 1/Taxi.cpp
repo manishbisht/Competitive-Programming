91c5f3d2eb17919203d3b6d064c54f38f5dd185e
Manish Bisht <manish.bisht490@gmail.com>
2017-03-04 22:10:15 +0530

    Codeforces solution updated

--- Codeforces/VK Cup 2012 Qualification Round 1/Taxi.cpp
+++ Codeforces/VK Cup 2012 Qualification Round 1/Taxi.cpp
@@ -2,18 +2,55 @@
 using namespace std;
 int main()
 {
-    long long int n, a, ans, i;
+    long long int n, ans, i, b, t, a[5];
+    for(i = 1; i < 5; ++i){
+        a[i] = 0;
+    }
     cin>>n;
-    ans = 0;
-    for(i = 0; i < n; i++){
-        cin>>a;
-        ans = ans + a;
+    for(i = 0; i < n; ++i){
+        cin>>b;
+        a[b]++;
     }
-    if(ans%4==0){
-        cout<<ans/4<<endl;
+    ans = a[4];
+    if(a[1] < a[3]){
+        ans += a[1];
+        a[3] -= a[1];
+        a[1] = 0;
     }
-    else{
-        cout<<(ans/4)+1<<endl;
+    else if(a[3] < a[1]){
+        ans += a[3];
+        a[1] -= a[3];
+        a[3] = 0;
     }
+    else {
+        ans += a[1];
+        a[1] = 0;
+        a[3] = 0;
+    }
+    if(a[1] > a[2]){
+        t = min(a[1] / 2, a[2]);
+        ans = ans + t;
+        a[1] -= t*2;
+        a[2] -= t;
+    }
+    /*if(a[1] < a[2]){
+        ans += a[1];
+        a[2] -= a[1];
+        a[1] = 0;
+    }
+    else if(a[2] < a[1]){
+        ans += a[2];
+        a[1] -= a[2];
+        a[2] = 0;
+    }
+    else {
+        ans += a[1];
+        a[1] = 0;
+        a[2] = 0;
+    }*/
+    ans += a[1];
+    ans += a[2];
+    ans += a[3];
+    cout<<ans<<endl;
     return 0;
 }
