59b98675075400ffd1f60fd7bc8f43cadef7aace
Manish Bisht <manish.bisht490@gmail.com>
2017-03-25 06:44:03 +0530

    Focuthon Solution Added

---
+++ Venturesity/Focuthon/Integer Size Comparison.py
@@ -0,0 +1,12 @@
+t = int(raw_input())
+for i in xrange(0, t):
+	a = long(raw_input())
+	b = long(raw_input())
+	c = long(raw_input())
+	d = long(raw_input())
+	if a ** b == c ** d:
+		print "E"
+	elif a ** b > c ** d:
+		print "Y"
+	else:
+		print "N"
