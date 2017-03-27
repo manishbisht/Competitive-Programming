1f97b9282bc9fd205fac7c5dc78ed237314c2583
Manish Bisht <manish.bisht490@gmail.com>
2017-03-27 19:38:44 +0530

    V Shape Solution added

---
+++ Venturesity/Focuthon/V Shape.py
@@ -0,0 +1,18 @@
+import sys
+S = sys.stdout.write
+X = xrange
+R = raw_input
+t = int(R())
+for i in X(0, t):
+	a = R().split(' ')
+	a[1] = int(a[1])
+	for j in X(0, a[1]):
+		b = a[1]-j-2
+		for k in X(0, j):
+			S(' ')
+		S(a[0])
+		for k in X(0, b):
+			S('  ')
+		if j != a[1] - 1:
+			S(' '+a[0])
+		S('\n')
