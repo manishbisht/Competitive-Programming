a6f333c538945cec72dcc6ff0bbb6b1c69179a9d
Manish Bisht <manish.bisht490@gmail.com>
2017-03-31 11:45:26 +0530

    Tic Tac Toe SOlution Added

---
+++ Venturesity/Focuthon/Tic Tac Toe.py
@@ -0,0 +1,43 @@
+t = int(raw_input())
+for i in xrange(0, t):
+	n = int(raw_input())
+	a = []
+	b = 0
+	c = 0
+	for j in xrange(0, n):
+		t = raw_input().split(' ')
+		a.append(t)
+	for j in xrange(0, n):
+		d = 0
+		e = 0
+		for k in xrange(0, n):
+			if a[j][k] == 'x':
+				d += 1
+				if e > 2:
+					c += e - 2
+				e = 0
+			else:
+				e += 1
+				if d > 2:
+					b += d - 2
+				d = 0
+	if e > 2:
+		c += e - 2
+	if d > 2:
+		b += d - 2
+	for j in xrange(0, n):
+		d = 0
+		e = 0
+		for k in xrange(0, n):
+			if a[k][j] == 'x':
+				d += 1
+				if e > 2:
+					c += e - 2
+				e = 0
+			else:
+				e += 1
+				if d > 2:
+					b += d - 2
+				d = 0
+	print b
+	print c
