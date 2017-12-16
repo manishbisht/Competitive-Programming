b1fc5079afc4c3477f53f8ff45d2f552585d201e
Manish Bisht <manish.bisht490@gmail.com>
2017-12-16 11:14:40 +0530

    Independence Day Cakes solution added

--- Skillenza (formly Venturesity)/Freedom Jam/Independence Day Cakes.py
+++ Skillenza (formly Venturesity)/Freedom Jam/Independence Day Cakes.py
@@ -1,3 +1,18 @@
+def isSubsetSum(arr, n, sum):
+    
+    subset = [ [False for j in range(sum + 1)] for i in range(3) ]
+    for i in range(n + 1):
+        for j in range(sum + 1):
+            if (j == 0):
+                subset[i % 2][j] = True
+            elif (i == 0):
+                subset[i % 2][j] = False
+            elif (arr[i - 1] <= j):
+                subset[i % 2][j] = subset[(i + 1) % 2][j - arr[i - 1]] or subset[(i + 1) % 2][j]
+            else:
+                subset[i % 2][j] = subset[(i + 1) % 2][j]
+    return subset[n % 2][sum]
+
 def check_subset(data, n, sum):
     subset = []
     for i in xrange(n + 1):
@@ -23,7 +38,7 @@ for i in xrange(t):
     m = int(raw_input().strip())
     if m in data:
         print "YES"
-    elif check_subset(data, n, m):
+    elif isSubsetSum(data, n, m):
         print "YES"
     else:
         print "NO"
