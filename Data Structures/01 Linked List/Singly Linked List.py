d487b6757086f8a3b4ea5aabec171c54d6574c6a
Manish Bisht <manish.bisht490@gmail.com>
2018-02-04 21:48:09 +0530

    LinkedList Insert

--- Data Structures/01 Linked List/Singly Linked List.py
+++ Data Structures/01 Linked List/Singly Linked List.py
@@ -32,7 +32,51 @@ class SinglyLinkedList:
     def __init__(self):
         self.head = None
 
-    # Return the length of the linked list
+    # Insert a new node in the LinkedList
+    def list_insert(self, position, data):
+        length_of_list = self.list_length()
+        if position > length_of_list or position < 0:
+            return None
+        elif position == 0:
+            self.insert_at_start(data)
+        elif position == length_of_list:
+            self.insert_at_end(data)
+        else:
+            new_node = Node()
+            new_node.set_data(data)
+            current = self.head
+            count = 0
+
+            while count < position - 1:
+                current = current.get_next()
+                count += 1
+
+            new_node.set_next(current.get_next())
+            current.set_next(new_node)
+
+    # Insert a new node at the start of LinkedList
+    def insert_at_start(self, data):
+        new_node = Node()
+        new_node.set_data(data)
+
+        if self.list_length() == 0:
+            self.head = new_node
+        else:
+            new_node.set_next(self.head)
+            self.head = new_node
+
+    # Insert a new node at the start of LinkedList
+    def insert_at_end(self, data):
+        new_node = Node()
+        new_node.set_data(data)
+        current = self.head
+
+        while current is not None:
+            current = current.get_next()
+
+        current.set_next(new_node)
+
+    # Return the length of the LinkedList
     def list_length(self):
         current = self.head
         count = 0
