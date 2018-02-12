af6652126ae741702b1f42c9cef8aeab4584fd6e
Manish Bisht <manish.bisht490@gmail.com>
2018-02-12 09:10:16 +0530

    Google Code Jam Template modified

--- Code Templates/Google.py
+++ Code Templates/Google.py
@@ -1,8 +1,9 @@
-f = open('A-small-practice.in')
-o = open('o1.txt', 'w')
+f = open('sample-input.txt')
+o = open('sample-output.txt', 'w')
 t = int(f.readline().strip())
 for i in xrange(1, t + 1):
-	n = int(f.readline().strip())
-	x = [int(j) for j in f.readline().strip().split(" ")]
-	y = [int(j) for j in f.readline().strip().split(" ")]
-	o.write("Case #{}:\n".format(i))
+    o.write("Case #{}: ".format(i))
+    n = int(f.readline().strip())
+    x = [int(j) for j in f.readline().strip().split()]
+    y = [int(j) for j in f.readline().strip().split()]
+    o.write("\n")
