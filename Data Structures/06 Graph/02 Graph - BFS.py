77af703fa99356de0f620fa9eeec104db08cfadc
Manish Bisht <manish.bisht490@gmail.com>
2018-02-12 15:50:52 +0530

    Graph DFS Adjacency Matrix added

--- Data Structures/06 Graph/02 Graph - BFS.py
+++ Data Structures/06 Graph/02 Graph - BFS.py
@@ -1,3 +1,53 @@
+# Adjacency Matrix Graph Class
+class Graph:
+    # Constructor
+    def __init__(self, size):
+        self.data = []
+        for i in xrange(size):
+            temp = []
+            for j in xrange(size):
+                temp.append(0)
+            self.data.append(temp)
+
+    # Add the vertex in the graph
+    def add_vertex(self, u, v):
+        self.data[u][v] = 1
+
+    # Print graph
+    def print_graph(self):
+        print self.data
+
+    # BFS
+    def bfs(self, source):
+        path = []
+        q = [source]
+        visited = []
+        for i in xrange(len(self.data)):
+            visited.append(0)
+        visited[source] = 1
+        path.append(source)
+        while len(q) > 0:
+            v = q.pop(0)
+            for i in xrange(len(self.data[v])):
+                if visited[i] == 0 and self.data[v][i] == 1:
+                    q.append(i)
+                    visited[i] = 1
+                    path.append(i)
+        return path
+
+AdjacencyMatrixGraph = Graph(8)
+AdjacencyMatrixGraph.print_graph()
+AdjacencyMatrixGraph.add_vertex(0, 1)
+AdjacencyMatrixGraph.add_vertex(0, 2)
+AdjacencyMatrixGraph.add_vertex(0, 3)
+AdjacencyMatrixGraph.add_vertex(1, 4)
+AdjacencyMatrixGraph.add_vertex(1, 5)
+AdjacencyMatrixGraph.add_vertex(2, 6)
+AdjacencyMatrixGraph.add_vertex(2, 7)
+AdjacencyMatrixGraph.add_vertex(3, 7)
+AdjacencyMatrixGraph.print_graph()
+print AdjacencyMatrixGraph.bfs(0)
+
 # Adjacency List Graph Class
 class ListGraph:
     # Constructor
@@ -33,15 +83,15 @@ class ListGraph:
         return path
 
 
-AdjacencyMatrixGraph = ListGraph(8)
-AdjacencyMatrixGraph.print_graph()
-AdjacencyMatrixGraph.add_vertex(0, 1)
-AdjacencyMatrixGraph.add_vertex(0, 2)
-AdjacencyMatrixGraph.add_vertex(0, 3)
-AdjacencyMatrixGraph.add_vertex(1, 4)
-AdjacencyMatrixGraph.add_vertex(1, 5)
-AdjacencyMatrixGraph.add_vertex(2, 6)
-AdjacencyMatrixGraph.add_vertex(2, 7)
-AdjacencyMatrixGraph.add_vertex(3, 7)
-AdjacencyMatrixGraph.print_graph()
-print AdjacencyMatrixGraph.bfs(0)
+AdjacencyListGraph = ListGraph(8)
+AdjacencyListGraph.print_graph()
+AdjacencyListGraph.add_vertex(0, 1)
+AdjacencyListGraph.add_vertex(0, 2)
+AdjacencyListGraph.add_vertex(0, 3)
+AdjacencyListGraph.add_vertex(1, 4)
+AdjacencyListGraph.add_vertex(1, 5)
+AdjacencyListGraph.add_vertex(2, 6)
+AdjacencyListGraph.add_vertex(2, 7)
+AdjacencyListGraph.add_vertex(3, 7)
+AdjacencyListGraph.print_graph()
+print AdjacencyListGraph.bfs(0)
