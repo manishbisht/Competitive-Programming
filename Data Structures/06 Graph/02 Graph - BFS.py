a0dc7e8fed434efc818a5be0d507546590cc5510
Manish Bisht <manish.bisht490@gmail.com>
2018-02-12 09:10:58 +0530

    Graph BFS modified

--- Data Structures/06 Graph/02 Graph - BFS.py
+++ Data Structures/06 Graph/02 Graph - BFS.py
@@ -51,10 +51,10 @@ print AdjacencyMatrixGraph.bfs(0)
 # Adjacency List Graph Class
 class ListGraph:
     # Constructor
-    def __init__(self, size):
+    def __init__(self, nodes):
         self.data = {}
-        for i in xrange(size):
-            self.data[i] = []
+        for node in nodes:
+            self.data[node] = []
 
     # Add the vertex in the Graph
     def add_vertex(self, u, v):
@@ -65,12 +65,12 @@ class ListGraph:
         print self.data
 
     # BFS
-    def bfs(self, source):
+    def bfs(self, source, nodes):
         path = []
         q = [source]
-        visited = []
-        for i in xrange(len(self.data)):
-            visited.append(0)
+        visited = {}
+        for node in nodes:
+            visited[node] = 0
         visited[source] = 1
         path.append(source)
         while len(q) > 0:
@@ -83,7 +83,7 @@ class ListGraph:
         return path
 
 
-AdjacencyListGraph = ListGraph(8)
+AdjacencyListGraph = ListGraph(range(8))
 AdjacencyListGraph.print_graph()
 AdjacencyListGraph.add_vertex(0, 1)
 AdjacencyListGraph.add_vertex(0, 2)
@@ -94,4 +94,4 @@ AdjacencyListGraph.add_vertex(2, 6)
 AdjacencyListGraph.add_vertex(2, 7)
 AdjacencyListGraph.add_vertex(3, 7)
 AdjacencyListGraph.print_graph()
-print AdjacencyListGraph.bfs(0)
+print AdjacencyListGraph.bfs(0, range(8))
