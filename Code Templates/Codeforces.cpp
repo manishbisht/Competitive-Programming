97767f32cb2442fdd577e4726da8d8a4152e197d
Maram Sumanth <maram.sumanth@gmail.com>
2019-10-07 18:18:52 +0530

    Update Codeforces.cpp

--- Code Templates/Codeforces.cpp
+++ Code Templates/Codeforces.cpp
@@ -1,12 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+#define M 1000000007
+#define PI 3.1415926
 #define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define all(v) v.begin(),v.end()
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+
+typedef priority_queue <int> pq;
 
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+	fastio;
 	
 	return 0;
 }
