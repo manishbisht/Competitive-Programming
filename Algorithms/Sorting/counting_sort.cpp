7d525849d25b37ed2a6ac40a421bb88b2e8b95b3
ananyag16 <56221316+ananyag16@users.noreply.github.com>
2019-10-11 23:14:03 +0530

    Create counting_sort.cpp

---
+++ Algorithms/Sorting/counting_sort.cpp
@@ -0,0 +1,40 @@
+//C++ Program for counting sort  
+#include<bits/stdc++.h>  
+#include<string.h> 
+using namespace std;  
+#define RANGE 255  
+  
+
+void countSort(char arr[])  
+{  
+
+    char output[strlen(arr)];  
+
+    int count[RANGE + 1], i;  
+    memset(count, 0, sizeof(count));  
+
+    for(i = 0; arr[i]; ++i)  
+        ++count[arr[i]];  
+ 
+    for (i = 1; i <= RANGE; ++i)  
+        count[i] += count[i-1];  
+
+    for (i = 0; arr[i]; ++i)  
+    {  
+        output[count[arr[i]]-1] = arr[i];  
+        --count[arr[i]];  
+    }  
+
+    for (i = 0; arr[i]; ++i)  
+        arr[i] = output[i];  
+}  
+int main()  
+{  
+    char arr[] = "geeksforgeeks"; 
+  
+    countSort(arr);  
+  
+    cout<< arr;  
+    return 0;  
+}  
+  
