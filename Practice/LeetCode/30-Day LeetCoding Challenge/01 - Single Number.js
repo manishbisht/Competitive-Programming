dfc73490a790177347bf176b52cd5f8d0100bfc7
Manish Bisht <manish.bisht490@gmail.com>
2020-04-03 11:03:35 +0530

    leetcode 30 days challenge started

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/01 - Single Number.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    result = 0
+    for (let i = 0; i < nums.length ; i++) {
+        result ^= nums[i];
+    }
+    return result
+};
