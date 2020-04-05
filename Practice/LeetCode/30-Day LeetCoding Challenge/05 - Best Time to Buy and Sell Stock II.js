8f601a11bc0c4998f574f7091d960bf601a0dbf2
Manish Bisht <manish.bisht490@gmail.com>
2020-04-05 16:51:01 +0530

    Best Time to Buy and Sell Stock solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/05 - Best Time to Buy and Sell Stock II.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let result = 0
+    for (let i = 1; i < prices.length; i++) {
+        let profit = prices[i] - prices[i - 1]
+        if (profit > 0) {
+            result += profit
+        }
+    }
+    return result
+};
