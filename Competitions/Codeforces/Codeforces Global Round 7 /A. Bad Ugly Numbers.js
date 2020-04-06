98d6f4afde3a7a15854f472ab8e4198a8ea9e9a4
Manish Bisht <manish.bisht490@gmail.com>
2020-04-06 14:25:08 +0530

    Group Anagrams solution added

--- Competitions/Codeforces/Codeforces Global Round 7 /A. Bad Ugly Numbers.js
+++ Competitions/Codeforces/Codeforces Global Round 7 /A. Bad Ugly Numbers.js
@@ -1,1 +1,19 @@
-let t = parseInt(readline())
+const readline = require("readline");
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+let lineNo = 0;
+let testCases;
+
+const parseProblem = (line) => {
+    if (lineNo === 0 ) {
+        testCases = parseInt(line)
+    }
+    lineNo++;
+};
+
+rl.on('line', function (line) {
+    // console.log('Line number: ' + line);
+    if (lineNo === 0) {
+
+    }
+});
---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/06 - Group Anagrams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const result = [];
+    const cache = {};
+    
+    for (let i = 0; i < strs.length; i++) {
+        let sortedStringKey = strs[i].split("").sort().join("")
+        if (cache[sortedStringKey]) {
+            result[cache[sortedStringKey] - 1].push(strs[i])
+        } else {
+            cache[sortedStringKey] = result.length + 1
+            result[result.length] = [strs[i]]
+        }
+    }
+    
+    return result
+};
