e85f7bc9e15c64957963cb6a01c81c7979e912e2
Manish Bisht <manish.bisht490@gmail.com>
2020-04-07 15:38:49 +0530

    Counting Elements solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/07 - Counting Elements.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var countElements = function(arr) {
+    let result = 0
+    const countMap = {}
+    for (let i = 0; i < arr.length; i++) {
+        if (countMap[arr[i]]) {
+            countMap[arr[i]] += 1
+        } else {
+            countMap[arr[i]] = 1
+        }
+    }
+    Object.keys(countMap).forEach(value => {
+        if (countMap[parseInt(value) + 1]) {
+            result += countMap[parseInt(value)]
+        }
+    })
+    return result
+};
