11f3448410e7c7af0108a295963cb69231898d06
Manish Bisht <manish.bisht490@gmail.com>
2020-04-08 15:36:00 +0530

    Middle of the Linked List solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/08 - Middle of the Linked List.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slowPointer = head
+    let fastPointer = head
+        
+    while (fastPointer.next && fastPointer.next.next) {
+        slowPointer = slowPointer.next
+        fastPointer = fastPointer.next.next
+    }
+    return fastPointer.next ? slowPointer.next : slowPointer
+};
