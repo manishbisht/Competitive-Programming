a4d1c8e74118a2f8395bc3957c41995ab7a3fbbd
Manish Bisht <manish.bisht490@gmail.com>
2020-04-09 18:51:15 +0530

    Backspace String Compare solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/09 - Backspace String Compare.js
@@ -0,0 +1,21 @@
+var processString = function(S) {
+    let processedString = ''
+    for (let i = 0; i < S.length; i++) {
+       if (S[i] === "#") {
+           if (processedString.length > 0) {
+               processedString = processedString.slice(0, -1); 
+           }
+       } else {
+          processedString += S[i] 
+       }
+    }
+    return processedString
+}
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {boolean}
+ */
+var backspaceCompare = function(S, T) {
+    return processString(S) === processString(T)
+};
