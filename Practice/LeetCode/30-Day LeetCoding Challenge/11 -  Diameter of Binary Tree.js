0ad918cba3745606dfa60b6eed2f48af23ba2b35
Manish Bisht <manish.bisht490@gmail.com>
2020-04-11 17:52:03 +0530

    Diameter of Binary Tree solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/11 -  Diameter of Binary Tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+var result 
+var treeHeight = function(root) {
+    if (root == null) {
+        return 0
+    }
+    
+    let leftHeight = treeHeight(root.left)
+    let rightHeight = treeHeight(root.right) 
+    
+    result = result > leftHeight + rightHeight + 1 ? result : leftHeight + rightHeight + 1
+    
+    return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1
+}
+
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {  
+    result = 1
+    treeHeight(root)
+    return result - 1
+};
