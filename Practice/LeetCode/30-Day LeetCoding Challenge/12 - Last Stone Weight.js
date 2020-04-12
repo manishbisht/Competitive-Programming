36072509968f1b0ddb358bd63a0d6449b31cb664
Manish Bisht <manish.bisht490@gmail.com>
2020-04-12 15:39:20 +0530

    Last Stone Weight solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/12 - Last Stone Weight.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    while (stones.length > 1) {
+        stones.sort((a, b) => b - a)
+        let a = stones.shift()
+        if (a == stones[0]) {
+            stones.shift()
+        } else {
+            stones[0] = a - stones[0]
+        }
+        console.log(stones)
+    }
+    
+    return stones.length > 0 ? stones[0] : 0
+};
