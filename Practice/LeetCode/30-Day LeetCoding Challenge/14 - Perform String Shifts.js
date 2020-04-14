256cb111c323ab7860376b5a2c449d582d8b8a82
Manish Bisht <manish.bisht490@gmail.com>
2020-04-14 14:42:00 +0530

    Perform String Shifts solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/14 - Perform String Shifts.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {number[][]} shift
+ * @return {string}
+ */
+var stringShift = function(s, shift) {
+    let shiftCount = 0
+    for (let i = 0; i < shift.length; i++) {
+        if (shift[i][0] == 0) {
+            shiftCount -= shift[i][1]
+        } else {
+            shiftCount += shift[i][1]
+        }
+    }
+    shiftCount = shiftCount % s.length
+    if (shiftCount > 0) {
+        return s.substring(s.length - shiftCount, s.length) + s.substring(0, s.length - shiftCount)
+    }
+    return s.substring(0 - shiftCount, s.length) + s.substring(0, 0 - shiftCount)
+};
