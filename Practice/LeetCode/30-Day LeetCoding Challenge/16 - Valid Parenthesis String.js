819c638865a29f1c0360de3224a0bcf8132d2a72
Manish Bisht <manish.bisht490@gmail.com>
2020-04-16 13:00:00 +0530

    Valid Parenthesis String solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/16 - Valid Parenthesis String.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkValidString = function(s) {
+    let left = 0
+    let right = 0
+    for (let i = 0; i < s.length; i++) {
+        left += s[i] == '(' ? 1 : -1
+        right += s[i] == ')' ? 1 : -1
+        if (right > 0) {
+            break;
+        }
+        left = left > 0 ? left : 0
+    }
+    return left == 0
+};
