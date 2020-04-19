921777044af3662a1204ead0843d24e08c102674
Manish Bisht <manish.bisht490@gmail.com>
2020-04-19 22:29:06 +0530

    Search in Rotated Sorted Array solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/19 - Search in Rotated Sorted Array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0
+    let right = nums.length - 1
+    while (left <= right) {
+        let mid = parseInt(left + ((right - left) / 2))
+        if (target == nums[mid]) {
+            return mid
+        }
+        if (nums[left] <= nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+    }
+    return -1
+};
