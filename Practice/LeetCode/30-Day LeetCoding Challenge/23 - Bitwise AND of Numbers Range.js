f394b577b781b1beab264d37a2f8d413283488d8
Manish Bisht <manish.bisht490@gmail.com>
2020-04-23 19:15:33 +0530

    Bitwise AND of Numbers Range solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/23 - Bitwise AND of Numbers Range.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var rangeBitwiseAnd = function(m, n) {
+    while (m < n) {
+        n -= (n & -n)
+    }
+    return n
+};
