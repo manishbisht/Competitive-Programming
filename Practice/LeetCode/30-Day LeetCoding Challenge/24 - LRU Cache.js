c71f55e11af5ff906d2b6052e86f38a546c3bf58
Manish Bisht <manish.bisht490@gmail.com>
2020-04-24 18:06:38 +0530

    LRU Cache solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/24 - LRU Cache.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.lastTrackedKeys = []
+    this.value = {}
+    this.capacity = capacity
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (this.lastTrackedKeys.includes(key)) {
+        this.lastTrackedKeys.splice(this.lastTrackedKeys.indexOf(key), 1);
+        this.lastTrackedKeys.unshift(key)
+    }
+    return this.value[key] || -1
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.lastTrackedKeys.includes(key)) {
+        this.lastTrackedKeys.splice(this.lastTrackedKeys.indexOf(key), 1);
+    }
+    
+    if (this.capacity == this.lastTrackedKeys.length) {
+        delete this.value[this.lastTrackedKeys[this.lastTrackedKeys.length - 1]]
+        this.lastTrackedKeys.splice(this.lastTrackedKeys.length - 1, 1);
+    }
+    
+    this.value[key] = value
+    this.lastTrackedKeys.unshift(key)
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
