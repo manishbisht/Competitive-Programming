9f271bc6335587c8b29a982ecc7ba656f89f562e
Manish Bisht <manish.bisht490@gmail.com>
2020-04-25 17:14:18 +0530

    Jump Game solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/25 - Jump Game.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    if (nums.length <= 1) {
+        return true;
+    }
+    
+    let ans = nums[0]
+    
+    for (let i = 0; i < nums.length; i++) {
+        if (ans <= i && nums[i] == 0) {
+            return false
+        }
+        
+        if (i + nums[i] > ans) {
+            ans = i + nums[i]
+        }
+        
+        if (ans >= nums.length - 1) {
+            return true
+        }
+    }
+    return false
+};
