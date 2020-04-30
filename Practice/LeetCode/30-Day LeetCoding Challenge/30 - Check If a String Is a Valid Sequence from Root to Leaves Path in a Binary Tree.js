9cc44dc779415b6632f8380ac13e102de39fdecb
Manish Bisht <manish.bisht490@gmail.com>
2020-04-30 18:42:26 +0530

    Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree solution added

---
+++ Practice/LeetCode/30-Day LeetCoding Challenge/30 - Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree.js
@@ -0,0 +1,27 @@
+var pathExists = function(root, arr, index) {
+    if (!root) {
+        return arr.length == 0
+    }
+    
+    if (!root.left && !root.right && root.val == arr[index] && index == arr.length - 1) {
+        return true
+    }
+    
+    return index < arr.length && root.val == arr[index] && (pathExists(root.left, arr, index + 1) || pathExists(root.right, arr, index + 1))
+}
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var isValidSequence = function(root, arr) {
+    return pathExists(root, arr, 0) || false
+};
