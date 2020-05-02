0b274c162507887ddecbfc09116bea76e5ae8a59
Manish Bisht <manish.bisht490@gmail.com>
2020-05-02 08:32:14 +0530

    First Bad Version solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/01 - First Bad Version.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let high = n
+        let low = 1
+        let ans = 1
+        while (low <= high) {
+            let mid = parseInt((low + high) / 2)
+            if (isBadVersion(mid)) {
+                if ((mid > 1 && !isBadVersion(mid - 1)) || mid == 1) {
+                    ans = mid
+                    break;
+                } else {
+                    high = mid - 1
+                }
+            } else {
+                low = mid + 1
+            }
+        }
+        return ans
+    };
+};
