980fefef946810d2677a24298bc9811430ba64d1
Manish Bisht <manish.bisht490@gmail.com>
2020-05-02 16:33:57 +0530

    Jewels and Stones solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/02 - Jewels and Stones.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    const count = {};
+    let ans = 0;
+    [...S].forEach(character => {
+        if (count[character]) {
+            count[character] += 1
+        } else {
+            count[character] = 1
+        }
+    });
+    
+    [...J].forEach(character => {
+        if (count[character]) {
+            ans += count[character]
+        }
+    });
+    
+    return ans
+};
