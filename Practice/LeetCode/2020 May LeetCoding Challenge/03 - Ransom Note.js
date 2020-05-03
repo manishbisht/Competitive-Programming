1d87963f48267a1f856b1b08e5bacbcb991f4139
Manish Bisht <manish.bisht490@gmail.com>
2020-05-03 20:19:19 +0530

    Ransom Note solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/03 - Ransom Note.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const count = {};
+    [...magazine].forEach((character) => {
+        if (count[character]) {
+            count[character] += 1
+        } else {
+            count[character] = 1
+        }
+    });
+    
+    let ans = true;
+    
+    [...ransomNote].forEach((character) => {
+        if (count[character]) {
+            count[character] -= 1
+        } else {
+            ans = false
+        }
+    });
+    
+    return ans;
+};
