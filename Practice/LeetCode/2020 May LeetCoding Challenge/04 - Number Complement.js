c5e75e4e7312734b5f075d0e2ec868b8f40d36c9
Manish Bisht <manish.bisht490@gmail.com>
2020-05-04 19:38:07 +0530

    Number Complement solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/04 - Number Complement.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function(num) {
+    let binaryRepresentation = (num >>> 0).toString(2);
+    let ans = 0;
+    
+    [...binaryRepresentation].forEach((value, index) => {
+        if (value == '0') {
+            ans += 2 ** (binaryRepresentation.length - index - 1)
+        }
+    });
+    
+    return ans
+};
