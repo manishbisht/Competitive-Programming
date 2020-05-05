784de315812f1b7ac9175d2439f10aadecb15a5e
Manish Bisht <manish.bisht490@gmail.com>
2020-05-05 19:33:07 +0530

    First Unique Character in a String solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/05 - First Unique Character in a String.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    const count = {};
+    let ans = -1;
+    [...s].forEach((character) => {
+        if (count[character]) {
+            count[character] += 1
+        } else {
+            count[character] = 1
+        }
+    });
+    
+    for (let i = 0; i < s.length; i++) {
+        if (count[s[i]] == 1) {
+            ans = i
+            break
+        }
+    }
+
+    return ans
+};
