d429dc7d5b64d78a4c1404e729bc90f3799fc28b
Manish Bisht <manish.bisht490@gmail.com>
2020-05-06 17:01:43 +0530

    Majority Element solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/06 - Majority Element.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    const count = {};
+    let ans = -1;
+    
+    [...nums].forEach((value) => {
+        if (count[value]) {
+            count[value] += 1;
+        } else {
+            count[value] = 1;
+        }
+        
+        if (count[value] > (nums.length / 2)) {
+            ans = value;
+        }
+    });
+    
+    return ans;
+};
