f4aad7f52d4c67011ba68ff212ac597da73c4a19
Manish Bisht <manish.bisht490@gmail.com>
2020-05-08 14:55:13 +0530

    Check If It Is a Straight Line solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/08 - Check If It Is a Straight Line.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    let ans = true;
+    let xChange = coordinates[0][0] - coordinates[1][0];
+    let yChange = coordinates[0][1] - coordinates[1][1];
+    let x = coordinates[0][0];
+    let y = coordinates[0][1];
+    coordinates.splice(0,2);
+    [...coordinates].forEach((coordinate) => {
+        if (xChange === 0) {
+            if (x !== coordinate[0]) {
+                ans = false;
+            }
+        } else if (yChange === 0) {
+            if (y !== coordinate[1]) {
+                ans = false;
+            }
+        } else if (xChange * (y - coordinate[1]) !== yChange * (x - coordinate[0])) {
+            ans = false;
+        }
+    });
+    return ans
+};
