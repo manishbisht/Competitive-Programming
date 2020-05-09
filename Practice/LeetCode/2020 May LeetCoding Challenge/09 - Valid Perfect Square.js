47fc66bfca6486d274b673a2facb6d09a33b0314
Manish Bisht <manish.bisht490@gmail.com>
2020-05-09 14:40:46 +0530

    Valid Perfect Square solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/09 - Valid Perfect Square.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let ans = false
+    for (let i = 1; i <= Math.ceil(num / 2); i++) {
+        if (i * i == num) {
+            ans = true;
+        }
+    }
+    return ans
+};
