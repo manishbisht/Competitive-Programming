19c0fed7c362fcaf3b08f1edf58fcf1d44cec77d
Manish Bisht <manish.bisht490@gmail.com>
2020-05-10 23:03:27 +0530

    Find the Town Judge solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/10 - Find the Town Judge.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} N
+ * @param {number[][]} trust
+ * @return {number}
+ */
+var findJudge = function(N, trust) {
+    const trustedBy = {};
+    const trustTo = {};
+    let ans = N == 1 && trust.length == 0 ? 1 : -1;
+    [...trust].forEach((trusted) => {
+        if (!trustedBy[trusted[1]]) {
+            trustedBy[trusted[1]] = {}
+        }
+        if (!trustTo[trusted[0]]) {
+            trustTo[trusted[0]] = {}
+        }
+        if (trusted[1] != trusted[0]) {
+            trustedBy[trusted[1]][trusted[0]] = 1
+            trustTo[trusted[0]][trusted[1]] = 1
+        }
+    });
+    Object.keys(trustedBy).forEach((person) => {
+        if (Object.keys(trustedBy[person]).length == N - 1 && !trustTo[person]) {
+            ans = person;
+        }
+    });
+    return ans;
+};
