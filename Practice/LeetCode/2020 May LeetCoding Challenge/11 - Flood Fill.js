e4bf94d4e3d38e9b3a32b2a3bf32aa3f06640bfd
Manish Bisht <manish.bisht490@gmail.com>
2020-05-11 19:28:58 +0530

    Flood Fill solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/11 - Flood Fill.js
@@ -0,0 +1,22 @@
+var fillColor = function(image, sr, sc, newColor, oldColor) {
+    if (sr < 0 || sr + 1 > image.length || sc < 0 || sc + 1 > image[0].length || image[sr][sc] != oldColor || image[sr][sc] == newColor) {
+        return image
+    }
+    image[sr][sc] = newColor
+    image = fillColor(image, sr + 1, sc, newColor, oldColor)
+    image = fillColor(image, sr - 1, sc, newColor, oldColor)
+    image = fillColor(image, sr, sc + 1, newColor, oldColor)
+    image = fillColor(image, sr, sc - 1, newColor, oldColor)
+    return image
+}
+
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} newColor
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, newColor) {        
+    return fillColor(image, sr, sc, newColor, image[sr][sc])
+};
