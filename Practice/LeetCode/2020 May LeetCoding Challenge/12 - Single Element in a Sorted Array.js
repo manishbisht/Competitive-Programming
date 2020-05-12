945fca8fbfcc985d3f29cf37b1719041dc9d0835
Manish Bisht <manish.bisht490@gmail.com>
2020-05-12 18:35:05 +0530

    Single Element in a Sorted Array solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/12 - Single Element in a Sorted Array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function(nums) {
+    let left = 0
+    let right = nums.length - 1
+    while (left < right) {
+        let mid = (left + right) / 2
+        if (mid % 2 == 0) {
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2
+            } else {
+                right = mid
+            }
+        } else {
+            if (nums[mid] == nums[mid - 1]) {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        } 
+    }
+    return nums[left]
+};
