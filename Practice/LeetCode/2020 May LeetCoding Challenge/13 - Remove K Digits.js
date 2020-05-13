5b9d4d5469577534090f0ce1dd61a23b524de507
Manish Bisht <manish.bisht490@gmail.com>
2020-05-13 21:22:31 +0530

    Remove K Digits solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/13 - Remove K Digits.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    num = num.split('')
+    if (num.length == k) {
+        return "0"
+    }
+    
+    for (let i = 0; i < k; i++) {
+        let j = 0
+        while (j + 1 < num.length && num[j] <= num[j + 1]) {
+            j++
+        }
+        num.splice(j, 1)
+    }
+    
+    while (num.length > 0 && num[0] == '0') {
+        num.splice(0, 1)
+    }
+    
+    if (num.length == 0) {
+        return "0"
+    }
+    
+    return num.join('')
+};
