08daa534f41a0c19ea538f4575243e26e82915bb
Manish Bisht <manish.bisht490@gmail.com>
2020-05-17 14:25:00 +0530

    Find All Anagrams in a String solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/17 - Find All Anagrams in a String.js
@@ -0,0 +1,49 @@
+var removeKey = function (count, key) {
+    if (count[key]) {
+        if (count[key] === 1) {
+            delete count[key]
+        } else {
+            count[key] -= 1
+        }
+    } else {
+        count[key] = -1
+    }
+    return count
+}
+
+var addKey = function(count, key) {
+    if (count[key]) {
+        if (count[key] == -1) {
+            delete count[key]
+        } else {
+            count[key] += 1
+        }
+    } else {
+        count[key] = 1
+    }
+    return count
+}
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+var findAnagrams = function(s, p) {
+    let count = {}
+    for (let i = 0; i < p.length; i++) {
+        count = addKey(count, s[i])
+    }
+    let countCheck = {...count};
+    for (let i = 0; i < p.length; i++) {
+        countCheck = removeKey(countCheck, p[i])
+    }
+    let result = Object.keys(countCheck).length == 0 ? [0] : [];
+    for (let i = 1; i <= s.length - p.length; i++) {
+        countCheck = removeKey(countCheck, s[i - 1])
+        countCheck = addKey(countCheck, s[i + p.length - 1])
+        if (Object.keys(countCheck).length == 0) {
+            result = [...result, i]
+        }
+    }
+    return result
+};
