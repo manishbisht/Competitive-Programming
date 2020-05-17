33b10d190fd295a66ce95e3be566043897ea1df7
Manish Bisht <manish.bisht490@gmail.com>
2020-05-17 09:47:02 +0530

    Odd Even Linked List solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/16 - Odd Even Linked List.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function(head) {
+    if (head) {
+        let result = head
+        let oddNode = head
+        let evenNode = head.next
+        let connectingNode = head.next
+        
+        while (oddNode != null && evenNode != null) {
+            if (evenNode.next == null) {
+                break
+            }
+            
+            oddNode.next = evenNode.next
+            oddNode = oddNode.next
+            
+            evenNode.next = oddNode.next
+            evenNode = evenNode.next
+        }
+        
+        oddNode.next = connectingNode
+        
+        return result
+    }
+    
+    return head
+};
