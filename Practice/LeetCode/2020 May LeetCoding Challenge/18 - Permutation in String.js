fb220fb76ccfe7a31b9ca5a3f84bf5e13ddc221e
Manish Bisht <manish.bisht490@gmail.com>
2020-05-18 18:53:05 +0530

    Permutation in String solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/18 - Permutation in String.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function(s1, s2) {
+    const count = {};
+    [...s1].forEach((character) => {
+        if (count[character]) {
+            count[character] += 1
+        } else {
+            count[character] = 1
+        }
+    });
+    let i = 0;
+    let j = 0;
+    let charCount = s1.length
+    while (j < s2.length) {
+        if (count[s2[j]] > 0) {
+            charCount -= 1
+        }
+        if (count[s2[j]]) {
+            count[s2[j]] -= 1
+        } else {
+            count[s2[j]] = -1
+        }
+        j += 1
+        if (charCount == 0) {
+            return true
+        }
+        if (j - i == s1.length) {
+            if (count[s2[i]] >= 0) {
+                charCount += 1
+            }
+            if (count[s2[i]]) {
+                count[s2[i]] += 1
+            } else {
+                count[s2[i]] = 1
+            }
+            i += 1
+        }
+    }
+    return false
+};
