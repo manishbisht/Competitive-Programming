faf526650563d50232254475962c30dd19f57540
Manish Bisht <manish.bisht490@gmail.com>
2020-05-19 16:34:13 +0530

    Online Stock Span solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/19 - Online Stock Span.js
@@ -0,0 +1,27 @@
+
+var StockSpanner = function() {
+    this.prices = []
+    this.spans = []
+};
+
+/** 
+ * @param {number} price
+ * @return {number}
+ */
+StockSpanner.prototype.next = function(price) {
+    let span = 1
+    let i = this.spans.length - 1
+    while (i >= 0 && price >= this.prices[i]) {
+        span += this.spans[i]
+        i -= this.spans[i]
+    }
+    this.spans.push(span)
+    this.prices.push(price)
+    return span
+};
+
+/** 
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
