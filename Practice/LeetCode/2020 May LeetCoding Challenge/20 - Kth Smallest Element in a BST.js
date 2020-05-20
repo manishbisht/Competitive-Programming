e5ef023a6d2dede3a1923a55cf32d107e2685598
Manish Bisht <manish.bisht490@gmail.com>
2020-05-20 16:44:53 +0530

    Kth Smallest Element in a BST solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/20 - Kth Smallest Element in a BST.js
@@ -0,0 +1,38 @@
+var count, ans;
+var inOrderTraversal = function(root) {
+    if (root == null) {
+        return
+    }
+    if (root.left) {
+        inOrderTraversal(root.left)
+    }
+    
+    count -= 1
+    
+    if (count == 0) {
+        ans = root.val
+    }
+        
+    if (root.right) {
+        inOrderTraversal(root.right)
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    count = k
+    inOrderTraversal(root)
+    return ans
+};
