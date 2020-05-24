2525deb6ffee3763fdc403cf76780cccb78efc39
Manish Bisht <manish.bisht490@gmail.com>
2020-05-24 16:58:11 +0530

    Construct Binary Search Tree from Preorder Traversal solution added

---
+++ Practice/LeetCode/2020 May LeetCoding Challenge/24 - Construct Binary Search Tree from Preorder Traversal.js
@@ -0,0 +1,36 @@
+var createNode = function(root, value) {
+    if (root) {
+        if (value > root.val) {
+            if (root.right) {
+               createNode(root.right, value) 
+            } else {
+                root.right = new TreeNode(value)
+            }
+        } else {
+            if (root.left) {
+               createNode(root.left, value) 
+            } else {
+                root.left = new TreeNode(value)
+            }
+        }
+    }
+}
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+var bstFromPreorder = function(preorder) {
+    let root = new TreeNode(preorder[0])
+    for (let i = 1; i < preorder.length; i++) {
+        createNode(root, preorder[i])
+    }
+    return root
+};
