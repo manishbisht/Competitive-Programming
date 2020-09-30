9d342b6d1449d1bdc644a100dd611120a09a04ad
Kushal Das <61356005+Kushal997-das@users.noreply.github.com>
2020-09-30 22:49:11 +0530

    Add files via upload

---
+++ Hackerrank_python/1.introduction/03.Arithmetic Operators.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print (a+b)
+    print (a-b)
+    print (a*b)
+
+    
---
+++ Hackerrank_python/1.introduction/04.Python_ Division.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+
+    
