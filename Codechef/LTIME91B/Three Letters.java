502a6d9050386b503fd858ccea9560092670c3b0
Sushank Sakthivel Nagarajan <60143563+Sushank34@users.noreply.github.com>
2021-08-24 22:03:15 +0530

    Create Three Letters.java

---
+++ Codechef/LTIME91B/Three Letters.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	static class FastReader 
+    { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() 
+        { 
+            br = new BufferedReader(new
+                     InputStreamReader(System.in)); 
+        } 
+  
+        String next() 
+        { 
+            while (st == null || !st.hasMoreElements()) 
+            { 
+                try
+                { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) 
+                { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() 
+        { 
+            return Integer.parseInt(next()); 
+        }
+        
+        
+        long nextLong() 
+        { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() 
+        { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() 
+        { 
+            String str = ""; 
+            try
+            { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) 
+            { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+    }
+	public static void main(String[] args) throws java.lang.Exception{
+		FastReader sc=new FastReader();
+		int t=sc.nextInt();
+		while(t-->0) {
+			String st=sc.next();
+			Dictionary dict = new Hashtable();
+			int sz=0,res=0;
+			for(int i=0;i<st.length();i++) {
+				if(dict.get(st.charAt(i))==null) {
+					dict.put(st.charAt(i),1);
+				}
+				else {
+					if(sz+3<=st.length()) {
+						sz=sz+3;
+						res++;
+						dict.remove(st.charAt(i));
+					}
+				}
+			}
+			System.out.println(res);
+		}
+	}
+}
