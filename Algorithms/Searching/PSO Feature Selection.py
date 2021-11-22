8d7be6620114b4fd3efd9d7a9c7c768190d62a2d
Dharaneedharan <ddsss1999@gmail.com>
2021-11-22 17:24:28 +0530

    Add accuracy computation method

--- Algorithms/Searching/PSO Feature Selection.py
+++ Algorithms/Searching/PSO Feature Selection.py
@@ -13,3 +13,33 @@ from sklearn.metrics import precision_score, recall_score
 
 import warnings
 warnings.filterwarnings('ignore')
+
+def classification_accuracy(y_actual, y_hat):
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+
+        for i in range(len(y_hat)): 
+            if y_actual[i]==y_hat[i]==1:
+                TP += 1
+            if y_hat[i]==1 and y_actual[i]!=y_hat[i]:
+                FP += 1
+            if y_actual[i]==y_hat[i]==0:
+                TN += 1
+            if y_hat[i]==0 and y_actual[i]!=y_hat[i]:
+                FN += 1
+               
+        class_acc = float((TP+TN)) / float((TP+FP+TN+FN))
+        
+        if TP == 0 and FN == 0 :
+            recall = 0
+        else:
+            recall  = float(TP) / float(TP + FN)
+        
+        if TP == 0 and FP == 0:
+            precision = 0
+        else:
+            precision = float(TP) / float( TP + FP )         
+                       
+        return (class_acc, recall, precision)
