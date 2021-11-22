e7536503812f680bbec40d5fc425071b905ecde0
Dharaneedharan <ddsss1999@gmail.com>
2021-11-22 17:27:27 +0530

    Compute Accuracy without feature selection

--- Algorithms/Searching/PSO Feature Selection.py
+++ Algorithms/Searching/PSO Feature Selection.py
@@ -43,3 +43,45 @@ def classification_accuracy(y_actual, y_hat):
             precision = float(TP) / float( TP + FP )         
                        
         return (class_acc, recall, precision)
+        
+def fitness_without_optimization(df1):
+  
+      # Separate labels and features
+      X = df1.drop(columns=['diagnosis'])
+      y = df1['diagnosis']
+      
+      # Convert the M to 1 and B to 0
+      label = LabelEncoder()
+      y = label.fit_transform(y)
+      y[:20]
+
+      # Spilt the train and test data
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+      # we used 30% test data
+    
+      # Logistic Regression
+      LR = LogisticRegression()
+      LR.fit(X_train, y_train)
+      LR.score(X_train, y_train)
+      y_pred = LR.predict(X_test)
+      y_pred_train = LR.predict(X_train)
+      
+      # find accuracy
+      ac = accuracy_score(y_test, y_pred)
+      ac_train = accuracy_score(y_train, y_pred_train)
+      # Code for ROC_AUC curve
+      rc = roc_auc_score(y_test, y_pred)
+      
+      cm_2 = confusion_matrix(y_test, y_pred)
+                
+      sns.heatmap(cm_2,annot=True,fmt="d")
+
+      class_acc = classification_accuracy(y_test, y_pred) 
+      
+      return class_acc
+      
+df = pd.read_csv('breast_cancer_data.csv')
+accuracy = fitness_without_optimization(df.copy())
+print('Accuracy  :' + "{:.2f}".format(accuracy[0]))
+print('Precision :' + "{:.2f}".format(accuracy[1]))
+print('Recall    :' + "{:.2f}".format(accuracy[2]))
